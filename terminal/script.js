e892aecc92426ded5153b5cabde146a9a62c2239
jules3182 <jtoad23@gmail.com>
2023-08-21 00:49:16 -0400

    Quick fix for mobile disabling autoselect

--- terminal/script.js
+++ terminal/script.js
@@ -1,5 +1,6 @@
 const inputField = document.querySelector('.input-field');
 const outputArea = document.querySelector('.output');
+const isDesktop = window.innerWidth > 768;
 
 inputField.addEventListener('keydown', function (event) {
     if (event.key === 'Enter') {
@@ -9,43 +10,44 @@ inputField.addEventListener('keydown', function (event) {
 
         // Handle different commands and redirects
 
-            // link back to main website page
+        // link back to main website page
         if (command === 'home' || command === 'main') {
             window.location.href = '../index.html';
 
             // some memes
-        }else if (command === 'top_secret') {
+        } else if (command === 'top_secret') {
             window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley';
 
             // link to reddit
-        }else if (command === 'heil_spez') {
+        } else if (command === 'heil_spez') {
             window.location.href = 'https://www.reddit.com/user/halfrican420';
 
             // Brings up a card page with all of my social medias on it, mostly for social junk
-        }else if (command === 'social') {
+        } else if (command === 'social') {
             //window.location.href = '';
 
-            // Yeahhh this is the help command.. if you're reading this deal with it because I have to aswell.
-        }else if (command === 'help') {
-            outputArea.innerHTML += '<p><strong>Oh, so you need help huh? very well.. I\'ll give in...</strong><br><br><strong class="halp">home or main</strong>: will bring you to the main landing page of my website<br><strong class="halp">social</strong>: Redirects to a page showing my various social media accounts to get in conntact with me <br> <strong class="halp">baked</strong>: Directly sends you to my art instagram <br><strong class="halp">heil_spez</strong>: Wanna see my reddit? Good luck.<br><strong class="halp">top_secret</strong>: Did you not read that?? It\'s <strong>TOP. SECRET.</strong> Understand?<br></p>';
 
             // link to baked blend ig
-        }else if (command === 'filler') {
+        } else if (command === 'filler') {
             //window.location.href = '';
             // outputArea.innerHTML += `';
 
             // link to baked blend ig
-        }else if (command === 'filler') {
+        } else if (command === 'filler') {
             //window.location.href = '';
             // outputArea.innerHTML += `';
 
             // link to baked blend ig
-        }else if (command === 'baked') {
+        } else if (command === 'baked') {
             window.location.href = 'https://www.instagram.com/baked.blend/';
+
+            // Yeahhh this is the help command.. if you're reading this deal with it because I have to aswell. It's obviously super inifient.. but its MY website not yours lol. Code it yourself
+        } else if (command === 'help') {
+            outputArea.innerHTML += '<p><strong>Oh, so you need help huh? very well.. I\'ll give in...</strong><br><br><strong class="halp">home or main</strong>: will bring you to the main landing page of my website<br><strong class="halp">social</strong>: Redirects to a page showing my various social media accounts to get in conntact with me <br> <strong class="halp">baked</strong>: Directly sends you to my art instagram <br><strong class="halp">heil_spez</strong>: Wanna see my reddit? Good luck.<br><strong class="halp">top_secret</strong>: Did you not read that?? It\'s <strong>TOP. SECRET.</strong> Understand?<br></p>';
         } else {
-    outputArea.innerHTML += `<p>Command not recognized: ${command}. Are you sure you typed that correctly?</p>`;
-}
-}
+            outputArea.innerHTML += `<p>Command not recognized: ${command}. Are you sure you typed that correctly?</p>`;
+        }
+    }
 });
 
 
@@ -69,7 +71,8 @@ document.addEventListener('DOMContentLoaded', function () {
     typeMessage(welcomeMessage, welcomeMessageElement);
 });
 
-//Auto select
-var input = document.getElementById('termIn');
-input.focus();
-input.select();
+if (isDesktop) {
+    var input = document.getElementById('termIn');
+    input.focus();
+    input.select();
+}
