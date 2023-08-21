df068e2a8f7a29648099260bb144d6d22d39a9b4
jules3182 <jtoad23@gmail.com>
2023-08-21 19:44:26 -0400

    Added a couple more commands as well as some styling

--- terminal/script.js
+++ terminal/script.js
@@ -18,7 +18,8 @@ const escapeMap = {
     '\\b': '<strong>',
     '\\e': '</strong>',
     '\\i': '<strong class="halp">',
-    '\\f': '</em>',
+    '\\h': '<strong class="heart">',
+    '\\f': '<strong class="tip">',
     '\\n': '<br><br>'
 };
 // tracks type message activity
@@ -27,7 +28,7 @@ let isTyping = false;
 
 document.addEventListener('DOMContentLoaded', function () {
     //  typing effect for the message
-    const welcomeMessage = "It seems you've stumbled apon my personal terminal.. Please, enter the command I gave you below to continue on to the correct path... Good luck, it was a pleasure meeting you <3";
+    const welcomeMessage = "\\bIt seems you've stumbled apon my personal terminal.. Please, enter the command I gave you below to continue on to the correct path... Good luck, it was a pleasure meeting you \\e\\h<3\\e \\n \\f(Feel free to use the help command to see all of your options!)\\e";
     const welcomeMessageElement = document.querySelector('.welcome-message');
 
     function typeMessage(text, element) {
@@ -56,7 +57,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 }
 
                 element.innerHTML = tagContent;
-
+                window.scrollTo(0, document.body.scrollHeight);
                 i++;
 
             } else {
@@ -119,6 +120,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
                 } else if (command === 'baker') {
                     // lol
+                    typeSpeed = 20;
                     typeMessage("My leige, welcome back. Allow me to get things right for you.", outputArea);
                     document.getElementById("username").innerHTML = 'Baker';
                     // Returns to main state
@@ -175,7 +177,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
                     // Brings up a card page with all of my social medias on it, mostly for social junk
                 } else if (command === 'social') {
-                    //window.location.href = '';
+                    typeMessage("\\b So you wanna get in contact then?\\e \\n \\i Instagram\\e: baked.blend\\n\\iSnapchat\\e: jb4339\\n\\iDiscord\\e: Halfrican420 \\n \\iSteam\\e: Halfrican420\\n \\iGithub\\e: Jules3182\\n", outputArea);
 
                     // Redirects to github page
                 } else if (command === 'h4ck3r_m0d3') {
@@ -213,12 +215,26 @@ document.addEventListener('DOMContentLoaded', function () {
 
                     // command to set text typing speed
                 } else if (command === 'set_speed') {
-                    typeMessage("There are 3 speeds to choose from. Slow, Medium, and Fast. Please enter in one of these values to apply.\\n", outputArea);
+                    typeMessage("There are 3 speeds to choose from. Slow, Medium, and Fast. Please enter in one of these values to apply or type \\iEXIT\\e to cancel.\\n", outputArea);
                     awaitingFollowUpInput = true;
                     FUspeed = true;
 
                 } else if (command.includes("yes")) {
                     typeMessage("Don't give me attitude, I'm only trying to help.\\n", outputArea);
+
+                    // Displays my current PC specs
+                } else if (command === 'specs') {
+                    //outputArea.innerHTML = '';
+                    //window.location.href = 'filer.html';
+
+                    // Shows the code from this website
+                } else if (command === 'show_code') {
+                    outputArea.innerHTML = 'https://github.com/Jules3182/jules3182.github.io/tree/master/terminal';
+
+                    // This is for me to be bale to copyy and quicly make new commands
+                } else if (command === 'filler') {
+                    //outputArea.innerHTML = '';
+                    //window.location.href = 'filer.html';
 
                     // This is for me to be bale to copyy and quicly make new commands
                 } else if (command === 'filler') {
@@ -229,13 +245,15 @@ document.addEventListener('DOMContentLoaded', function () {
                     // Yeahhh this is the help command.. if you're reading this deal with it because I have to aswell. It's obviously super inifient.. but its MY website not yours lol. Code it yourself
                 } else if (command === 'help') {
                     typeSpeed = 10;
-                    typeMessage("\\b Oh, so you need help huh? very well.. I\'ll give in... \\e \\n \\i home \\e: will bring you to the main landing page of my website\\n\\imain\\e: Same as home\\n\\isocial\\e: Redirects to a page showing my various social media accounts to get in conntact with me \\n \\ibaked\\e: Directly sends you to my art instagram \\n\\iheil_spez\\e: You really wanna see my reddit? Welp, good luck.\\n\\itop_secret\\e: Did you not read that?? It\'s \\bTOP. SECRET.\\e Understand?\\n\\ih4ck3r_m0d3\\e: This redirects you to my github page\\n\\i set_user\\e: Prompts you to change the username of who is logged in\\n\\iclear\\e: Clears the console\\n\\ilight_mode\\e: Toggles between light and dark mode in the console\\n \\iset_speed\\e: Allows you to adjust the typing speed\\n", outputArea);
+                    typeMessage("\\b Oh, so you need help huh? very well.. I\'ll give in... \\e \\n \\i home\\e or \\i main\\e: will bring you to the main landing page of my website\\n\\isocial\\e: Reads off my various social media accounts to get in contact with me \\n \\ibaked\\e: Directly sends you to my art instagram \\n\\iheil_spez\\e: You really wanna see my reddit? Welp, good luck.\\n\\itop_secret\\e: Did you not read that?? It\'s \\bTOP. SECRET.\\e Understand?\\n\\ih4ck3r_m0d3\\e: This redirects you to my github page\\n\\i set_user\\e: Prompts you to change the username of who is logged in\\n\\iclear\\e: Clears the console\\n\\ilight_mode\\e: Toggles between light and dark mode in the console\\n \\iset_speed\\e: Allows you to adjust the typing speed\\n\\i specs\\e: Shows the specs of my current PC build\\n\\i show_code\\e: Brings you to the code base of this page\\n\\i help\\e: homie... this is help you know what it does\\n", outputArea);
                     typeSpeed = 40;
+                    //     \\i COMMAND\\e: DESCRIPTION\\n
+
                     // Command not recognized state
                 } else {
                     typeMessage("Command not recognized: " + command + ". Are you sure you typed that correctly?\\n", outputArea);
                     setTimeout(function () {
-                        typeMessage("You can always enter help to get a list of commands.", outputArea);
+                        typeMessage("You can also always enter help to get a list of commands.", outputArea);
                     }, 5000);
                 }
             }
