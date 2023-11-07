651132da77de91d99efd1da2a52d7297762add3d
jules3182 <jtoad23@gmail.com>
2023-11-07 16:23:52 -0500

    Added the portfolio page and it actually kinda works

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,1 +1,18 @@
-// Script to automatically pull images from my drive folder to show on my portfolio... hopefully lol
+const categoryButtons = document.querySelectorAll('.category-buttons button');
+const gridItems = document.querySelectorAll('.grid-item');
+
+categoryButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    const selectedCategory = button.getAttribute('data-category');
+
+    gridItems.forEach((item) => {
+      const itemCategories = item.getAttribute('data-category').split(' ');
+
+      if (selectedCategory === 'all' || itemCategories.includes(selectedCategory)) {
+        item.style.display = 'block';
+      } else {
+        item.style.display = 'none';
+      }
+    });
+  });
+});
