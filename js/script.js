402e0cabf3c54cc7af250f5f2da84dcd833dbd81
jules3182 <jtoad23@gmail.com>
2023-11-07 22:52:15 -0500

    More tests to get the portfolio locked down as well as hopefully yet another mobile fix

--- js/script.js
+++ js/script.js
@@ -1,50 +1,53 @@
-window.onload = function() {
-/* global document */
-// Get the modal
-var modal = document.getElementById("pgpModal");
+window.onload = function () {
+    /* global document */
+    // Get the modal
+    var modal = document.getElementById("pgpModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("ModalBtn");
+    // Get the button that opens the modal
+    var btn = document.getElementById("ModalBtn");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal
-btn.onclick = function () {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-//window.onclick = function(event) {
-//  if (event.target == modal) {
-//    modal.style.display = "none";
-//  }
-//}
-
-
-
-// Please, reload the page if it doesn't works properly <3
-// See the console to check the number of <hr> tags
-
-let hrElement;
-let counter = 100;
-for (let i = 0; i < counter; i++) {
-    hrElement = document.createElement("HR");
-    if (i == counter - 1) {
-        hrElement.className = "thunder";
-    } else {
-        hrElement.style.left = Math.floor(Math.random() * window.innerWidth - 300) + "px";
-        hrElement.style.animationDuration = 0.2 + Math.random() * 0.3 + "s";
-        hrElement.style.animationDelay = Math.random() * 5 + "s";
+    // When the user clicks on the button, open the modal
+    btn.onclick = function () {
+        modal.style.display = "block";
     }
-    document.body.appendChild(hrElement);
-}
 
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    //window.onclick = function(event) {
+    //  if (event.target == modal) {
+    //    modal.style.display = "none";
+    //  }
+    //}
+
+    function isDesktop() {
+        return window.innerWidth >= 1079;
+    }
+
+    // Please, reload the page if it doesn't works properly <3
+    // See the console to check the number of <hr> tags
+
+    let hrElement;
+    let counter = 100;
+    if (isDesktop()) {
+    for (let i = 0; i < counter; i++) {
+        hrElement = document.createElement("HR");
+        if (i == counter - 1) {
+            hrElement.className = "thunder";
+        } else {
+            hrElement.style.left = Math.floor(Math.random() * window.innerWidth - 300) + "px";
+            hrElement.style.animationDuration = 0.2 + Math.random() * 0.3 + "s";
+            hrElement.style.animationDelay = Math.random() * 5 + "s";
+        }
+        document.body.appendChild(hrElement);
+    }
+}
 console.log(
     "There are " +
     document.querySelectorAll("hr").length +
